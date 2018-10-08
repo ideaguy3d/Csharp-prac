c932e1c0c3945168361de7a73eac46d4fe16ee2f
Julius Alvarado <angularjs.uiux@gmail.com>
2018-10-07 22:50:42 -0700

    renamed file

---
+++ PlayerCtrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+    public float thrust;
+    public RigidBody rb;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<RigidBody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
