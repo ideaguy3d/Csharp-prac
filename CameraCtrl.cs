1e36e85a9b8400c350422c2e45406c18dc704608
Julius Alvarado <angularjs.uiux@gmail.com>
2018-10-09 01:02:45 -0700

    added user interface text for points and win text

--- CameraCtrl.cs
+++ CameraCtrl.cs
@@ -15,8 +15,8 @@ public class CameraCtrl : MonoBehaviour {
 	
     private void LateUpdate()
     {
+
         transform.position = player.transform.position + offset;
 
-        // transform.position += offset; 
     }
 }
--- PlayerCtrl.cs
+++ PlayerCtrl.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerCtrl : MonoBehaviour {
 
     public float speed;
     private Rigidbody rb;
-
+    private int count;
+    public Text countText;
+    public Text winText;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = " "; 
     }
 
     void FixedUpdate()
@@ -43,8 +50,19 @@ public class PlayerCtrl : MonoBehaviour {
         if(otherCollider.gameObject.CompareTag("Pick Up"))
         {
             otherCollider.gameObject.SetActive(false);
+            count++;
+            SetCountText(); 
         }
 
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count Text: " + count.ToString();
+        if(count >= 6)
+        {
+            winText.text = "You Win !! \\^_^/";
+        }
     }
 
 }
